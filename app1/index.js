4a3bcb49ea07922289f93dd7a32b8bd164cdaef9
Nagabhushanam <nagabhushanam.n@revature.com>
2023-02-14 10:57:10 +0530

    fetch api

--- app1/index.js
+++ app1/index.js
@@ -14,3 +14,35 @@ hideBtn.addEventListener("click", (e) => {
 showBtn.addEventListener("click", (e) => {
   box.style.display = "block";
 });
+
+// using DOM API + Timer API
+
+setInterval(() => {
+  const timeNowEle = document.querySelector("#time-now");
+  timeNowEle.innerText = new Date().toLocaleTimeString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
+}, 1000);
+
+// using DOM API + Fetch API
+
+const top5TodosBtn = document.querySelector("#top5-todos");
+const todosBodyEle = document.querySelector("#todos-body");
+top5TodosBtn.addEventListener("click", (e) => {
+  const url = `https://jsonplaceholder.typicode.com/todos?_limit=5`;
+  let promise = fetch(url);
+  promise
+    .then((response) => response.json())
+    .then((todos) => {
+      let rows = todos.map((todo) => {
+        return `
+            <tr class="${todo.completed ? "bg-success" : ""}">
+                <td>${todo.id}</td>
+                <td>${todo.title}</td>
+                <td>${todo.completed}</td>
+            </tr>
+        `;
+      });
+      todosBodyEle.innerHTML = rows.join(" ");
+    });
+});
